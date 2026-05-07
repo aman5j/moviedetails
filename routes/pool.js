a53b7f57467659f2e374e22bda2f510f8b7653e8
aman5j <amanprajapati190@gmail.com>
2026-05-07 11:02:01 +0530

    aiven database setup

--- routes/pool.js
+++ routes/pool.js
@@ -1,13 +1,31 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
+// var pool = mysql.createPool({
+//     user:'root',
+//     host:'localhost',
+//     port:3306,
+//     password:'1234',
+//     database:'moviedetails',
+//     multipleStatements:true,
+//     connectionLimit:100
+// })
+
+// module.exports = pool;
+
+require('dotenv').config(); // Loads the variables from .env
+var mysql = require("mysql2");
+
 var pool = mysql.createPool({
-    user:'root',
-    host:'localhost',
-    port:3306,
-    password:'1234',
-    database:'moviedetails',
-    multipleStatements:true,
-    connectionLimit:100
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, //parseInt(process.env.DB_PORT)
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+    connectionLimit: 100,
+    ssl: {
+        rejectUnauthorized: false 
+    }
+});
 
 module.exports = pool;
 
